8300db331def4f545bcaa3cf5e1a418cced5506e
Jason Claeys <jdclaeys@outlook.com>
2024-05-05 22:59:59 -0500

    add new quote page

--- views/quotes/index.js
+++ views/quotes/index.js
@@ -5,6 +5,170 @@ const newQuotePage = () => {
         <main>
             <section>
                 <h1 class="title is-size-1">Get A Free Quote</h1>
+                <form action="/quotes" method="POST">
+                    <div class="field">
+                        <label for="firstName" class="label">
+                            First Name
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="firstName" name="firstName" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="lastName" class="label">
+                            Last Name
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="lastName" name="lastName" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="businessName" class="label">
+                            Business Name
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="businessName" name="businessName" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="streetAddressOne" class="label">
+                            Street Address One
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="streetAddressOne" name="streetAddressOne" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="streetAddressTwo" class="label">
+                            Street Address Two
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="streetAddressTwo" name="streetAddressTwo" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="city" class="label">
+                            City
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="city" name="city" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="state" class="label">
+                            State
+                        </label>
+                        <div class="control">
+                            <div class="select">
+                                <select>
+                                    <option>Alabama</option>
+                                    <option>Alaska</option>
+                                    <option>Arizona</option>
+                                    <option>Arkansas</option>
+                                    <option>California</option>
+                                    <option>Colorado</option>
+                                    <option>Connecticut</option>
+                                    <option>Delaware</option>
+                                    <option>Florida</option>
+                                    <option>Georgia</option>
+                                    <option>Hawaii</option>
+                                    <option>Idaho</option>
+                                    <option>Illinois</option>
+                                    <option>Indiana</option>
+                                    <option>Iowa</option>
+                                    <option>Kansas</option>
+                                    <option>Kentucky</option>
+                                    <option>Louisiana</option>
+                                    <option>Maine</option>
+                                    <option>Maryland</option>
+                                    <option>Massachusetts</option>
+                                    <option>Michigan</option>
+                                    <option>Minnesota</option>
+                                    <option>Mississippi</option>
+                                    <option>Missouri</option>
+                                    <option>Montana</option>
+                                    <option>Nebraska</option>
+                                    <option>Nevada</option>
+                                    <option>New Hampshire</option>
+                                    <option>New Jersey</option>
+                                    <option>New Mexico</option>
+                                    <option>New York</option>
+                                    <option>North Carolina</option>
+                                    <option>North Dakota</option>
+                                    <option>Ohio</option>
+                                    <option>Oklahoma</option>
+                                    <option>Oregon</option>
+                                    <option>Pennsylvania</option>
+                                    <option>Rhode Island</option>
+                                    <option>South Carolina</option>
+                                    <option>South Dakota</option>
+                                    <option>Tennessee</option>
+                                    <option>Texas</option>
+                                    <option>Utah</option>
+                                    <option>Vermont</option>
+                                    <option>Virginia</option>
+                                    <option>Washington</option>
+                                    <option>West Virginia</option>
+                                    <option>Wisconsin</option>
+                                    <option>Wyoming</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="zipcode" class="label">
+                            Zip Code
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="zipCode" name="zipCode" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="phoneNumber" class="label">
+                            Phone Number
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="phoneNumber" name="phoneNumber" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="email" class="label">
+                            Email
+                        </label>
+                        <div class="control">
+                            <input class="input" type="text" id="email" name="email" />
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="projectType" class="label">
+                            Project Type
+                        </label>
+                        <div class="control">
+                            <div class="select">
+                                <select>
+                                    <option></option>
+                                    <option></option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="projectDetails" class="label">
+                            Project Details
+                        </label>
+                        <div class="control">
+                            <textarea class="textarea"></textarea>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label for="budget" class="label">
+                            Budget
+                        </label>
+                        <div class="control">
+                            <input class="input" type="number" id="budget" name="budget" />
+                        </div>
+                    </div>
+                </form>                
             </section>
         </main>
     ` })
