5315cd247617cb1c3833720c98ce63ce391764ad
Jason Claeys <jdclaeys@outlook.com>
2024-05-08 10:38:20 -0500

    add property to admin model and create doc model

--- models/Admin.js
+++ models/Admin.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const AdminSchema = new Schema({
+  name: String,
   email: String,
   password: String
 })
---
+++ models/Doc.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const DocSchema = new Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'Admin',
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    image: {
+        path: String,
+        filename: String
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Doc = mongoose.model('Doc', DocSchema)
+
+export default Doc
