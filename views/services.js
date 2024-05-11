1f18cfc3a6eecf6b247968d7c5a89a3ac0fc7a4f
Jason Claeys <jdclaeys@outlook.com>
2024-05-10 23:03:19 -0500

    adjust styling on services page

--- views/services.js
+++ views/services.js
@@ -3,6 +3,9 @@ import layout from "./layout.js"
 const servicesPage = () => {
     return layout({ template: `
         <main class="container">
+            <div>
+                <h1 class="title is-size-1 mb-6">Services</h1>
+            </div>
             <div class="columns">
                 <div class="column">
                     <div class="columns">
@@ -10,24 +13,24 @@ const servicesPage = () => {
                             <div class="card mb-4">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_websites.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 1
+                                        Websites
                                     </div>
                                 </div>
                             </div>
                             <div class="card">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_web_applications.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 2
+                                        Web Applications
                                     </div>
                                 </div>
                             </div>
@@ -36,24 +39,24 @@ const servicesPage = () => {
                             <div class="card mb-4">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_mobile_applications.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 3
+                                        Mobile Applications
                                     </div>
                                 </div>
                             </div>
                             <div class="card">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_desktop_applications.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 4
+                                        Desktop Applications
                                     </div>
                                 </div>
                             </div>
@@ -66,24 +69,24 @@ const servicesPage = () => {
                             <div class="card mb-4">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_augmented_reality.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 5
+                                        Augmented Reality
                                     </div>
                                 </div>
                             </div>
                             <div class="card">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_virtual_reality.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 6
+                                        Virtual Reality
                                     </div>
                                 </div>
                             </div>
@@ -92,24 +95,24 @@ const servicesPage = () => {
                             <div class="card mb-4">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_blockchain.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 7
+                                        Blockchain
                                     </div>
                                 </div>
                             </div>
                             <div class="card">
                                 <div class="card-image">
                                     <figure class="image is-4by3">
-                                        <img src="" />
+                                        <img src="/resources/images/services_artificial_intelligence.png" />
                                     </figure>
                                 </div>
                                 <div class="card-content">
                                     <div class="content">
-                                        Service 8
+                                        Artificial Intelligence
                                     </div>
                                 </div>
                             </div>
