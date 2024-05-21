60d56a35c10d01fd6957ce698fbc5aa6e9c0601c
Jason Claeys <jdclaeys@outlook.com>
2024-05-20 20:42:32 -0500

    send products object with HTML to products index page

--- controllers/products/index.js
+++ controllers/products/index.js
@@ -1,7 +1,15 @@
 import 'dotenv/config'
 import { validationResult } from 'express-validator'
+import Product from '../../models/Product.js'
 import newProductPage from '../../views/products/new.js'
+import productsPage from '../../views/products/index.js'
 
 export const getNew = (req, res, next) => {
     res.send(newProductPage({}, req))
+}
+
+export const getProducts = async (req, res, next) => {
+    const products = await Product.find({})
+
+    res.send(productsPage({ products }))
 }
