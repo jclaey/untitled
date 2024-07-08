247a59dcd6dbaa17abbdf047a916d9c160283afc
Jason Claeys <jdclaeys@outlook.com>
2024-07-08 11:54:08 -0500

    add order model

---
+++ models/Order.js
@@ -0,0 +1,88 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const OrderSchema = new Schema({
+    orderItems: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            qty: {
+                type: Number,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            },
+            product: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: "Product"
+            }
+        }
+    ],
+    shippingAddress: {
+        streetAddressOne: {
+            type: String,
+            required: true
+        },
+        streetAddressTwo: String,
+        city: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        }
+    },
+    paymentMethod: {
+        type: String,
+        required: true
+    },
+    paymentResult: {
+        id: String,
+        status: String,
+        updateTime: String,
+        emailAddress: String
+    },
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    paidAt: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Order = mongoose.model('Order', OrderSchema)
+
+export default Order
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ import { fileURLToPath } from 'node:url'
 import express from 'express'
 import cookieSession from 'cookie-session'
 import methodOverride from 'method-override'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
 import index from './routes/index.js'
 import admin from './routes/admin/index.js'
 import quotes from './routes/quotes/index.js'
 import docs from './routes/docs/index.js'
 import products from './routes/products/index.js'
 import services from './routes/services/index.js'
+import users from './routes/users/index.js'
 import connectDB from './config/db.js'
 
 connectDB()
@@ -37,6 +40,13 @@ app.use(cookieSession({
   sameSite: 'strict'
 }))
 
+app.use(session({
+  secret: 'lklekaiudbfip32n48dpa3pihirgldnagf3qp3r09ieemviej',
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  resave: false,
+  saveUninitialized: true
+}))
+
 // Mount routes
 app.use('/', index)
 app.use('/admin', admin)
@@ -44,6 +54,7 @@ app.use('/quotes', quotes)
 app.use('/docs', docs)
 app.use('/products', products)
 app.use('/services', services)
+app.use('/users', users)
 
 const PORT = process.env.PORT || 3000
 
