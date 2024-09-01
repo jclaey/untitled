0aff3e3d42193db43e85a0c14c56a61da363bd40
Jason Claeys <jdclaeys@outlook.com>
2024-08-31 21:18:20 -0500

    add links to footer

--- views/partials/footer.js
+++ views/partials/footer.js
@@ -3,14 +3,16 @@ const footer = () => {
         <div class="mb-3 footer">
             <div class="columns">
                 <div class="column">
-                    <h1 class="is-size-3">Web Solutions</h1>
+                    <a href="/">
+                        <h1 class="is-size-3">Web Solutions</h1>
+                    </a>
                 </div>
                 <div class="column">
                     <div>
-                        <a href="/about" target="_blank" class="has-text-dark is-size-5">About Us</a>
+                        <a href="/about" class="has-text-dark is-size-5">About Us</a>
                     </div>
                     <div>
-                        <a href="/contact" target="_blank" class="has-text-dark is-size-5">Contact Us</a>
+                        <a href="/contact" class="has-text-dark is-size-5">Contact Us</a>
                     </div>
                     <div>
                         <a href="#" target="_blank" class="has-text-dark is-size-5">Terms and Conditions</a>
